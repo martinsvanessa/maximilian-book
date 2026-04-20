208569453043c8bdb64a6b67c2a639219eaee606
Gislene Lima <vann.n.a0044@gmail.com>
2026-04-20 12:36:37 -0300

    initial presentation

---
+++ practice-1/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+<>
+   <header>
+        <img src={'https://avatars.githubusercontent.com/u/18190437'} alt="An image of Gi" />
+        <h1>Gislene Lima</h1>
+        <p>Web developer and technology enthusiast</p>
+      </header>
+      <main>
+        <p>Right now, I am 32 years old and I live in Teresina.</p>
+        <p>
+          My full name is Gislene Lima and I am a web developer.
+        </p>
+      </main>
+      </>
+  )
+}
+
+export default App
